bcf0f5f0d47e061e0f106c3a9905dabb47a59436
Do Huynh <huynhdoo@gmail.com>
2025-07-09 12:57:59 +0000

    feat: add score post route

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
 from classifier import Classifier
@@ -15,7 +15,21 @@ model = Classifier("2025-05-27 offres_ft_rf.joblib")
 
 @app.route("/")
 def hello():
-    return "Hello from your Dockerized Flask app! Environnement: " + env + ""
+    # return "Hello from your Dockerized Flask app! Environnement: " + env + ""
+    return "LBA classifier API ready."
+
+@app.route('/score', methods = ['POST'])
+def score():
+    # print("Received request:", request)
+    if request.is_json:
+        # Get the JSON data
+        data = request.get_json() 
+        print("Received data:", data)
+        text = data['text']
+        return jsonify(model.score(text)), 200
+    else:
+        # If the request did not contain JSON data, return an error
+        return jsonify({'error': 'Request must be JSON'}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
