6877476d4197f6202fe40af333e8d20380afc1e0
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2026-03-03 14:30:34 +0100

    fix: release

--- release.config.js
+++ release.config.js
@@ -13,6 +13,7 @@ module.exports = {
     [
       "semantic-release-slack-bot",
       {
+        packageName: "labonnealternance-lab",
         notifyOnSuccess: true,
         notifyOnFail: true,
         markdownReleaseNotes: true,
