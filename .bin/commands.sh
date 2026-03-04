d722453187f0c136d40e96a9e815b129a66129ac
Mathieu Borrell <mathieu.borrell@beta.gouv.fr>
2026-03-04 12:07:12 +0100

    feat: migration d'Ansible Vault vers SOPS

--- .bin/commands.sh
+++ .bin/commands.sh
@@ -2,77 +2,45 @@
 
 set -euo pipefail
 
-function Help() {
-   echo "Commands"
-   echo "  bin:setup                                  Installs mna-lab binary with zsh completion on system"
-   echo "  init:env                                   Update local env files using values from vault file"
-   echo "  release:interactive                        Build & Push Docker image releases"
-   echo "  release:app                                Build & Push Docker image releases"
-   echo "  deploy <env> --user <your_username>        Deploy application to <env>"
-   echo "  vault:init                                 Fetch initial vault-password from template-apprentissage"
-   echo "  vault:edit                                 Edit vault file"
-   echo "  vault:password                             Show vault password"
-   echo "  deploy:log:encrypt                         Encrypt Github ansible logs"
-   echo "  deploy:log:dencrypt                        Decrypt Github ansible logs"
-   echo "  log:encrypt <log_file>                     Encrypt log file"
-   echo "  log:decrypt <encrypted_log_file>           Decrypt log file"
-   echo
-   echo
+if [ ! -f "${ROOT_DIR}/.bin/shared/commands.sh" ]; then
+
+  echo "Mise à jour du sous-module mna-shared-bin"
+
+  git submodule update --init "${ROOT_DIR}/.bin/shared"
+
+fi
+
+. "${ROOT_DIR}/.bin/shared/commands.sh"
+
+unset _meta_help["seed:update"]
+unset seed:update
+unset _meta_help["seed:apply"]
+unset seed:apply
+
+################################################################################
+# Non-shared commands
+################################################################################
+
+_meta_help["app:build"]="Build Ui & Server Docker images"
+
+function app:build() {
+  "${SCRIPT_DIR}/app-build.sh" "$@"
 }
 
-function bin:setup() {
-  sudo ln -fs "${ROOT_DIR}/.bin/mna-lab" "/usr/local/bin/mna-lab"
+_meta_help["app:release"]="Build & push Docker image releases"
 
-  sudo mkdir -p /usr/local/share/zsh/site-functions
-  sudo ln -fs "${ROOT_DIR}/.bin/zsh-completion" "/usr/local/share/zsh/site-functions/_mna-lab"
-  sudo rm -f ~/.zcompdump*
+function app:release() {
+  "${SCRIPT_DIR}/app-release.sh" "$@"
 }
 
-function init:env() {
-  "${SCRIPT_DIR}/setup-local-env.sh" "$@"
+_meta_help["app:release:interactive"]="Interactivelly build & push Docker image releases"
+
+function app:release:interactive() {
+  "${SCRIPT_DIR}/app-release-interactive.sh" "$@"
 }
 
-function release:interactive() {
-  "${SCRIPT_DIR}/release-interactive.sh" "$@"
+_meta_help["env:init"]="Update local env files using values from SOPS files"
+
+function env:init() {
+  "${SCRIPT_DIR}/env-init.sh" "$@"
 }
-
-function release:app() {
-  "${SCRIPT_DIR}/release-app.sh" "$@"
-}
-
-function deploy() {
-  "${SCRIPT_DIR}/deploy-app.sh" "$@"
-}
-
-function vault:init() {
-  # Ensure Op is connected
-  op account get > /dev/null
-  op document get ".vault-password-tmpl" --vault "${OP_VAULT_NAME}" > "${ROOT_DIR}/.infra/vault/.vault-password.gpg"
-}
-
-function vault:edit() {
-  editor=${EDITOR:-'code -w'}
-  EDITOR=$editor "${SCRIPT_DIR}/edit-vault.sh" "$@"
-}
-
-function vault:password() {
-  "${SCRIPT_DIR}/get-vault-password-client.sh" "$@"
-}
-
-function deploy:log:encrypt() {
-  (cd "$ROOT_DIR" && "${SCRIPT_DIR}/deploy-log-encrypt.sh" "$@")
-}
-
-function deploy:log:decrypt() {
-  (cd "$ROOT_DIR" && "${SCRIPT_DIR}/deploy-log-decrypt.sh" "$@")
-}
-
-function log:encrypt() {
-  "${SCRIPT_DIR}/log-encrypt.sh" "$@"
-}
-
-function log:decrypt() {
-  "${SCRIPT_DIR}/log-decrypt.sh" "$@"
-}
-
-
